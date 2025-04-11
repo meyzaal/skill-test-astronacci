87f6fc02f85c70178f563de2317f16e7dda6f028
meyzaal <almayza17@gmail.com>
2025-04-11 22:32:52 +0700

    feat: add Flutter mobile app with multi-platform support (Android, iOS, Web, Windows, macOS)

---
+++ user_app_mobile/android/app/src/main/kotlin/com/example/verygoodcore/user/app/mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.user_app_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ user_app_mobile/lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:user_app_mobile/app/app.dart';
+import 'package:user_app_mobile/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
